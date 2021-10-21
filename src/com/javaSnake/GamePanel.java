cbb4be002530a9b6a0a5d90d9e0dc1d4b2c02839
Charity Rogers <rogerscl116@gmail.com>
2021-10-21 16:16:27 -0500

    add the snake

--- src/com/javaSnake/GamePanel.java
+++ src/com/javaSnake/GamePanel.java
@@ -43,12 +43,25 @@ public class GamePanel extends JPanel implements ActionListener {
         draw(g);
     }
     public void draw(Graphics g) {
+        // the grid
         for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
         }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        // the snake
+        for(int i = 0; i < bodyParts; i++) {
+            if(i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
     public void newApple() {
         appleX = random.nextInt((int)((SCREEN_WIDTH/UNIT_SIZE))* UNIT_SIZE);
