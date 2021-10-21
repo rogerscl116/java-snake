c407ce583b75319ebf4cd15655ee05182a3d6a1d
Charity Rogers <rogerscl116@gmail.com>
2021-10-21 15:34:49 -0500

    add grid to game

--- src/com/javaSnake/GamePanel.java
+++ src/com/javaSnake/GamePanel.java
@@ -43,7 +43,10 @@ public class GamePanel extends JPanel implements ActionListener {
         draw(g);
     }
     public void draw(Graphics g) {
-
+        for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
     }
     public void newApple() {
 
